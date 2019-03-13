ba95665c78d9e2cc7da01d6208bd7d2b7b86e43d
admin admin <evaladmin@example.com>
2019-03-13 00:42:05 -0400

    User admin wrote content /scripts/rest/products.get.groovy

--- scripts/rest/products.get.groovy
+++ scripts/rest/products.get.groovy
@@ -1,30 +1,7 @@
 def results = [:]
-
-/*
-results = [ notebooks: [
-      [
-        name: '123 Notebook Lenovo Ideapad 320 Intel® Core i5-7200u 8GB',
-        price: 2259,
-        image: 'https://images-americanas.b2w.io/produtos/01/00/item/132381/3/132381386G1.png',
-        stars: 5,
-        totalReviews: 230,
-        details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
-      ],
-      [
-        name: 'Notebook Dell Inspiron i15-3567-A30P Intel Core 7ª i5 4GB',
-        price: 2284,
-        image: 'https://images-americanas.b2w.io/produtos/01/00/item/133280/7/133280747G1.jpg',
-        stars: 3.4,
-        totalReviews: 20,
-        details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
-      ]
- 	]
- ]
- */
  
-//results.notebooks = siteItemService.getSiteTree("/site/components/products/notebooks", 1)
 results.notebooks = itemsToProducts(siteItemService.getSiteTree("/site/components/products/notebooks", 1))
-//results.smartphones = itemsToProducts(siteItemService.getSiteTree("/site/components/products/smartphones", 1))
+results.smartphones = itemsToProducts(siteItemService.getSiteTree("/site/components/products/smartphones", 1))
 
 return results
 
@@ -34,7 +11,7 @@ def itemsToProducts(items) {
     items.getChildItems().each { item ->
     	def product = [:]
         product.name = item.queryValue("name")
-        product.details = item.queryValue("detail")
+        product.details = item.queryValue("details")
         product.price = item.queryValue("price")
         product.image = item.queryValue("image")
         product.stars = 5 // we'll do this later
