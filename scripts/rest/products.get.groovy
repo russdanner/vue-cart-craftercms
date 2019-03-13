d725f46d3c9b06e5ee82eaca7a63ee789accce39
admin admin <evaladmin@example.com>
2019-03-12 22:17:22 -0400

    User admin wrote content /scripts/rest/products.get.groovy

---
+++ scripts/rest/products.get.groovy
@@ -0,0 +1,22 @@
+def results = [:]
+
+results = [
+      [
+        name: 'Notebook Lenovo Ideapad 320 Intel® Core i5-7200u 8GB',
+        price: 2259,
+        image: 'https://images-americanas.b2w.io/produtos/01/00/item/132381/3/132381386G1.png',
+        stars: 5,
+        totalReviews: 230,
+        details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+      ],
+      [
+        name: 'Notebook Dell Inspiron i15-3567-A30P Intel Core 7ª i5 4GB',
+        price: 2284,
+        image: 'https://images-americanas.b2w.io/produtos/01/00/item/133280/7/133280747G1.jpg',
+        stars: 3.4,
+        totalReviews: 20,
+        details: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industrys standard dummy text ever since the 1500s',
+      ]
+ ]
+
+ return results
